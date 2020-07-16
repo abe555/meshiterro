bf7730b6b4c54fd0b45216574ddcd53e2b83c03b
abe555 <riho.has3v@gmail.com>
2020-07-16 19:12:05 +0900

    [FINISH]5-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9052867917061a512b6a7718a1d3bbc8f8886f1a899bd68eb452f9fbfc51b4a545bedb92f4ff494391e9abb7a22b2d2cd781a3eed4208b3f6f13cbdaec831b6a'
